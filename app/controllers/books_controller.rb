2ffc65236cc6ae21e929e17092d6d562c3d647ba
EC2 Default User <ec2-user@ip-172-31-47-178.ap-northeast-1.compute.internal>
2023-01-28 08:51:16 +0000

    [Update] レイアウト調整、validation追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,51 @@
 class BooksController < ApplicationController
-  def new
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully!"
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      flash[:notice] = "error!"
+      render :index
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully edited!"
+      redirect_to "/books/#{@book.id}"
+    else
+      flash[:notice] = "error!"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "successfully destroyed"
+      redirect_to "/books"
+    end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
